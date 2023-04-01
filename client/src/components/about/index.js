64e476d064b5594c7d63fa2a681011655cbc9486
Mark Green Jr <mrgssg@gmail.com>
2023-04-01 11:08:40 -0400

    added about component

---
+++ client/src/components/about/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function About(props) {
+    return (
+        <div className= "about" id="about">
+            {/* adding info about the site and us */}
+            </div>
+    )
+}
+
+export default About;
