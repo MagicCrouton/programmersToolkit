78864dd3236924f79a8dcdb335548ef033d570bc
eugene park <e.park5336@gmail.com>
2023-04-02 19:41:08 -0400

    started addding openAI packages and included navbar in header component

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import Navbar from '../Navbar'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
+    <div>
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link className="text-light" to="/">
           <h1 className="m-0">Programmer's ToolKit</h1>
         </Link>
       </div>
+      <Navbar />
     </header>
+
+    </div>
   );
 };
 
