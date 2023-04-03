57cbf14a85f3940eabac444916f623a6504929f0
eugene park <e.park5336@gmail.com>
2023-04-02 20:37:39 -0400

    exported the aifetch module

--- server/utils/aiFetch.js
+++ server/utils/aiFetch.js
@@ -1,22 +1,21 @@
 require('dotenv').config()
 const { Configuration, OpenAIApi } = require('openai');
 
-console.log(process.env.API_KEY)
 const configuration = new Configuration({
     apiKey: process.env.API_KEY
 })
 
 const openai = new OpenAIApi(configuration);
 
-const ping = async () => {
+const aiFetch = async () => {
 const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
+    prompt: "make a basic html header",
+    max_tokens: 100,
+    temperature: .5,
   });
 
-console.log(response.data.choices.text)
+// console.log(response.data.choices[0].text)
 }
 
-ping()
+module.exports= {aiFetch}
