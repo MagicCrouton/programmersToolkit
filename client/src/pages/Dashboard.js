f9462aa87e957c711746b21d49871f6dde756f3c
eugene park <e.park5336@gmail.com>
2023-04-16 13:58:58 -0400

    consolidating pages in dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import AuthServices from '../utils/auth';
 
-import './dashboard.css'
-import ProjectList from '../components/ProjectList';
-
 import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_ME, QUERY_ME } from '../utils/queries';
 
+import './dashboard.css'
+import NewProjectForm from '../components/NewProjectForm';
+import ProjectList from '../components/ProjectList';
+import ProjectMain from '../components/ProjectMain';
+import AppNavBar from '../components/AppNavBar/Navbar';
 
 
 
@@ -17,32 +19,20 @@ const Dashboard = () => {
   const handlePageChange = (page) => setCurrentPage(page);
   const renderPage = () => {
     if (currentPage === 'ViewProjects') {
-      return <Home handlePageChange={handlePageChange} />;
+      return <ProjectList />;
     }
     if (currentPage === 'CreateNewProject') {
-      return <About />;
+      return <NewProjectForm />;
     }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Resume') {
-      return <Resume />;
-    }
-    return <Contact />; 
   };
 
   return (
     <div>
         <h1>Welcome back, {AuthServices.getProfile().data.username}!</h1>
-        <button onClick={() => {window.location.assign('/projectList')} }>
-           View/Edit Your Projects
-        </button>
-        <br></br>
-        <br></br>
-        <button onClick={() => {window.location.assign('/newproject')} }>
-           Create a New Project
-        </button>
-        <ProjectList />
+        <div>
+        <AppNavBar />
+        {renderPage()}
+        </div>
     </div>
   );
 };
