a0af811d0341afdac0bd5a941cd97cd7dd4e643a
eugene park <e.park5336@gmail.com>
2023-04-16 21:51:14 -0400

    minor changes and cleanup

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,12 +1,12 @@
 import './ProjectList.css';
 
 import React from 'react';
+import ProjectRender from './projectRender';
 import { QUERY_ME, QUERY_PROJECTMAIN } from '../../utils/queries';
 import { FIND_SINGLE_PROJECT, REMOVE_PROJECT } from '../../utils/mutations';
 import { useQuery, useMutation } from '@apollo/client';
 
 function ProjectList({handlePageChange}) {
-
     const {loading, data} = useQuery(QUERY_ME);
     // const {loadingView, viewdata} = useQuery(QUERY_PROJECTMAIN);
     // const [viewProject] = useMutation(FIND_SINGLE_PROJECT)
@@ -20,7 +20,6 @@ function ProjectList({handlePageChange}) {
                 projectId: projectId
             }
         })
-
       handlePageChange('ViewProjects');
     }
 
@@ -39,29 +38,7 @@ function ProjectList({handlePageChange}) {
         <h3 className="text-primary">Your Projects</h3>
       </div>
       <div id="projects" className="flex-row justify-space-between my-4">
-        {userData.projects.map((project) => (
-            <div key={project._id} className="col-12 col-xl-6">
-              <div className="card mb-3">
-                <h4 className="card-header bg-dark text-light p-2 m-0">
-                  {project.projectName} <br />
-                  <span className="text-white" style={{ fontSize: '1rem' }}>
-                    Project Description: {project.projectDescription ? project.projectDescription : 0}{' '}
-                   
-                    {project.projectDescription && project.projectDescription === 1 ? '' : ''}
-                  </span>
-                </h4>
-                <div className="card-body bg-light p-2">
-              <p>{project.initialCode}</p>
-              <button onClick={() => {handleDelete(project._id)} }>
-                Delete
-              </button>
-              <button onClick={() => {handleView(project._id)}  }>
-                                    View Project
-              </button>
-            </div>
-              </div>
-            </div>
-          ))}
+    <ProjectRender userData={userData} handleDelete={handleDelete} handleView={handleView} />
       </div>
     </div>
   );
